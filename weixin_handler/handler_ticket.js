26d6d6d04b113b549f5336d5361e2566b8c3c8e2
岳大禹 <yuedayu13@gmail.com>
2015-12-05 05:35:42 +0800

    抢票内核完全移植到缓存上。

--- weixin_handler/handler_ticket.js
+++ weixin_handler/handler_ticket.js
@@ -5,19 +5,23 @@ var urls = require("../address_configure");
 var checker = require("./checkRequest");
 var basicInfo = require("../weixin_basic/settings.js");
 
+var cache = require('../models/ticket_cache.js');
+var all_activity = cache.all_activity;
+var current_activity = cache.current_activity;
+
 //Attentez: keep the activity::key unique globally.
 var TICKET_DB = model.tickets;
 var USER_DB = model.students;
 var ACTIVITY_DB = model.activities;
-var SEAT_DB = model.seats;
+//var SEAT_DB = model.seats;
 var db = model.db;
 var redis_db = model.redis_db;
 
-var alphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789";
+var alphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789_+-()<>!@#$%^&*";
 
 var act_cache = {};
-var rem_cache = {};
-var tik_cache = {};
+//var rem_cache = {};
+//var tik_cache = {};
 var usr_lock = {};
 
 exports.clearCache = function () {
@@ -25,7 +29,7 @@ exports.clearCache = function () {
 };
 
 function verifyStudent(openID, ifFail, ifSucc) {
-    redis_db.get(USER_DB + '_' + openID, function(err, res) {
+    redis_db.get(USER_DB + '_' + openID, function (err, res) {
         if (err || !res) {
             ifFail();
             return;
@@ -34,130 +38,71 @@ function verifyStudent(openID, ifFail, ifSucc) {
     });
 }
 exports.verifyStu = verifyStudent;
+
 function verifyActivities(actKey, ifFail, ifSucc) {
     var timer = new Date();
     var current = timer.getTime();
-    var theAct = act_cache[actKey];
-    if (theAct) {
-        if (current < theAct.book_end) {
-            if (current > theAct.book_start) {
-                ifSucc(theAct._id, theAct);
-                return;
+    var book_start = 0;
+    var activity = current_activity[actKey];
+    if (activity == null) {
+        for (var x in all_activity) {
+            if (all_activity[x].activity_key == actKey) {
+                book_start = all_activity[x].book_start;
+                break;
             }
-            ifFail(theAct.book_start - current);
+        }
+        if (book_start == 0) {
+            ifFail();
             return;
         }
-        act_cache[actKey] = null;
-        tik_cache[actKey] = null;
-        ifFail();
-        return;
+        ifFail(book_start - current);
+    } else if (activity.status < 0 || activity.status > 0) { // not start
+        book_start = all_activity[activity.activity_id].book_start;
+        ifFail(book_start - current);
+    } else {
+        ifSucc(activity.activity_id, all_activity[activity.activity_id]);
     }
-    db[ACTIVITY_DB].find(
-        {
-            key: actKey,
-            book_end: {$gt: current},
-            status: 1
-        }, function (err, docs) {
-            if (err || docs.length == 0) {
-                ifFail();
-                return;
-            }
-            act_cache[actKey] = docs[0];
-            if (current > docs[0].book_start) {
-                ifSucc(docs[0]._id, docs[0]);
-                return;
-            }
-            else {
-                ifFail(docs[0].book_start - current);
-                if (tik_cache[actKey] == null) {
-                    lock.acquire("rem_tik_fetcher", function () {
-                        if (tik_cache[actKey] != null) {
-                            lock.release("rem_tik_fetcher");
-                            return;
-                        }
-                        tik_cache[actKey] = {};
-                        tik_cache[actKey].tikMap = {};
-                        tik_cache[actKey].usrMap = {};
-                        lock.release("rem_tik_fetcher");
-                        return;
-                    });
-                }
-                return;
-            }
-        });
 }
-function getRandomString() {
-    var ret = "";
 
-    for (var i = 0; i < 13; i++)
+function getRandomString(len) {
+    var ret = "";
+    for (var i = 0; i < len; i++)
         ret += alphabet[Math.floor(Math.random() * alphabet.length)];
     return ret;
 }
+
+function transTime(tt) {
+    var res = "";
+    var len = alphabet.length;
+    while (tt > 0) {
+        res += alphabet[tt % len];
+        tt /= len;
+        tt = Math.floor(tt);
+    }
+    return res;
+}
+
 //Attentez: this function can only be called on condition that the collection is locked.
-function generateUniqueCode(callback, prefix, actKey) {
-    while (true) {
-        var tickCode = prefix + "_" + getRandomString();
-        if (tik_cache[actKey].tikMap[tickCode] == null) {
-            callback(tickCode);
-            return;
-        }
-    }
+function generateUniqueCode(stu_id, act_id, tic_index) {
+    var res = "";
+    res += transTime(parseInt(stu_id));
+    res += (act_id.toString()).substr(14);
+    res += transTime((new Date()).getTime());
+    res += tic_index.toString(36);
+    res += getRandomString(32 - res.length);
+    return res;
 }
-function presentTicket(msg, res, tick, act) {
-    var tmp = "恭喜，抢票成功！\n";
-    tmp += template.getHyperLink("点我查看电子票", urls.ticketInfo + "?ticketid=" + tick.unique_id);
+
+function presentTicket(msg, res, user_tickets, act) {
+    var tmp = "恭喜，抢票成功！您现在有" + user_tickets.num + "张票:\n";
+    var i = 1;
+    for (var x in user_tickets.tickets) {
+        tmp += template.getHyperLink("点我查看第" + i + "张电子票", urls.ticketInfo + "?ticketid="
+            + user_tickets.tickets[x].unique_id);
+    }
     if (act.need_seat != 0)
-        tmp += "\n注意:该活动需在抢票结束前选座(区)，请进入电子票选择。";
+        tmp += "\n注意:选座（区）将在抢票结束一小时之后截止，请尽快前往电子票选座。";
     res.send(template.getPlainTextTemplate(msg, tmp));
-}
-function fetchRemainTicket(key, callback) {
-    if (rem_cache[key] != null) {
-        callback();
-        return;
-    }
-    lock.acquire("rem_tik_fetcher", function () {
-        if (rem_cache[key] != null) {
-            lock.release("rem_tik_fetcher");
-            callback();
-            return;
-        }
-        db[ACTIVITY_DB].find(
-            {
-                key: key,
-                status: 1
-            }, function (err, docs) {
-                if (err || docs.length == 0) {
-                    lock.release("rem_tik_fetcher");
-                    return;
-                }
-                if (tik_cache[key] == null) {
-                    tik_cache[key] = {};
-                    tik_cache[key].tikMap = {};
-                    tik_cache[key].usrMap = {};
-                    db[TICKET_DB].find({activity: docs[0]._id}, function (err2, docs2) {
-                        if (err2) {
-                            lock.release("rem_tik_fetcher");
-                            return;
-                        }
-                        for (var i = 0; i < docs2.length; i++) {
-                            tik_cache[key].tikMap[docs2[i].unique_id] = true;
-                            if (docs2[i].status != 0)
-                                tik_cache[key].usrMap[docs2[i].stu_id] = true;
-                        }
-                        rem_cache[key] = docs[0].remain_tickets;
-                        lock.release("rem_tik_fetcher");
-                        callback();
-                        return;
-                    });
-                }
-                else {
-                    rem_cache[key] = docs[0].remain_tickets;
-                    lock.release("rem_tik_fetcher");
-                    callback();
-                    return;
-                }
-            });
-    });
 }
 
 function getTimeFormat(timeInMS) {
@@ -184,21 +129,33 @@ exports.check_get_ticket = function (msg) {
         if (msg.Content[0] === "抢票" || msg.Content[0].substr(0, 3) === "抢票 ")
             return true;
     return false;
-}
+};
+
 exports.faire_get_ticket = function (msg, res) {
     var actName, openID;
 
     if (msg.MsgType[0] === "text") {
         if (msg.Content[0] === "抢票") {
-            res.send(template.getPlainTextTemplate(msg, "请使用“抢票 活动代称”的命令或菜单按钮完成指定活动的抢票。"));
+            res.send(template.getPlainTextTemplate(msg,
+                "请使用“抢票 活动代称 [抢票张数（可选，阿拉伯数字）]”的命令或菜单按钮完成指定活动的抢票。"));
             return;
-        }
-        else {
+        } else {
             actName = msg.Content[0].substr(3);
         }
-    }
-    else {
+    } else {
         actName = msg.EventKey[0].substr(basicInfo.WEIXIN_BOOK_HEADER.length);
+    }
+
+    var part = actName.split(" ");
+    actName = part[0];
+    var ticket_num = 1;
+    if (part.length > 1) {
+        ticket_num = parseInt(actName[1]);
+    }
+    if (!ticket_num) {
+        res.send(template.getPlainTextTemplate(msg,
+            "无法解析抢票张数；请使用“抢票 活动代称 [抢票张数（可选，阿拉伯数字）]”的命令或菜单按钮完成指定活动的抢票。"));
+        return;
     }
 
     openID = msg.FromUserName[0];
@@ -215,63 +172,54 @@ exports.faire_get_ticket = function (msg, res) {
             if (tl == null)
                 res.send(template.getPlainTextTemplate(msg, "目前没有符合要求的活动处于抢票期。"));
             else
-                res.send(template.getPlainTextTemplate(msg, "该活动将在 " + getTimeFormat(tl) + " 后开始抢票，请耐心等待！"));
+                res.send(template.getPlainTextTemplate(msg, "该活动将在" + getTimeFormat(tl) + "后开始抢票，请耐心等待！"));
         }, function (actID, staticACT) {
-            fetchRemainTicket(actName, function () {
-                //Attentez: unlike stuID which is THUid, act id is simply act._id
-                if (tik_cache[actName].usrMap[stuID] != null) {
-                    res.send(template.getPlainTextTemplate(msg, "你已经有票啦，请用查票功能查看抢到的票吧！"));
-                    return;
+            if (staticACT.activity_info.remain_tickets < ticket_num) {
+                res.send(template.getPlainTextTemplate(msg, "对不起，只剩下"
+                    + staticACT.activity_info.remain_tickets + "张票啦\n(╯‵□′)╯︵┻━┻。"));
+                return;
+            }
+            var user_info = staticACT.user_map[stuID];
+            if (!user_info) {
+                staticACT.user_map[stuID] = {
+                    num: 0,
+                    tickets: {}
+                };
+                user_info = staticACT.user_map[stuID];
+            }
+            if (user_info.num + ticket_num >= staticACT.activity_info.max_tickets) {
+                res.send(template.getPlainTextTemplate(msg,
+                    "你已经有" + user_info.num + "张票啦，每个人最多" + staticACT.activity_info.max_tickets
+                    + "张票哦，再抢会炸的！"));
+                return;
+            }
+            if (usr_lock[stuID] != null) {
+                res.send(template.getPlainTextTemplate(msg, "您的抢票请求正在处理中，请稍后通过查票功能查看抢票结果(/▽＼)"));
+                return;
+            }
+            usr_lock[stuID] = true;
+            lock.acquire('cache' + staticACT.activity_info.key, function () {
+                for (var i = 0; i < ticket_num; i++) {
+                    staticACT.activity_info.remain_tickets -= 1;
+                    staticACT.user_map[stuID].num += 1;
+                    var unique_tic_code = generateUniqueCode(stuID, actID, staticACT.user_map[stuID].num);
+                    staticACT.user_map[stuID].tickets[unique_tic_code] = {
+                        stu_id: stuID,
+                        unique_id: unique_tic_code,
+                        activity: actID,
+                        status: 1,
+                        seat: "",
+                        cost: (staticACT.need_seat == 2 ? parseInt(staticACT.price) : 0)
+                    };
                 }
-                else {
-                    if (usr_lock[stuID] != null) {
-                        res.send(template.getPlainTextTemplate(msg, "您的抢票请求正在处理中，请稍后通过查票功能查看抢票结果(/▽＼)"));
-                        return;
-                    }
-                    usr_lock[stuID] = "true";
-
-                    if (rem_cache[actName] == 0) {
-                        usr_lock[stuID] = null;
-                        res.send(template.getPlainTextTemplate(msg, "对不起，票已抢完...\n(╯‵□′)╯︵┻━┻。"));
-                        return;
-                    }
-                    rem_cache[actName]--;
-                    db[ACTIVITY_DB].update(
-                        {
-                            _id: actID
-                        },
-                        {
-                            $inc: {remain_tickets: -1}
-                        }, {multi: false}, function (err, result) {
-                            if (err || result.n == 0) {
-                                usr_lock[stuID] = null;
-                                res.send(template.getPlainTextTemplate(msg, "(╯‵□′)╯︵┻━┻"));
-                                return;
-                            }
-                            var ss = actID.toString();
-                            generateUniqueCode(function (tiCode) {
-                                tik_cache[actName].tikMap[tiCode] = true;
-                                tik_cache[actName].usrMap[stuID] = true;
-                                db[TICKET_DB].insert(
-                                    {
-                                        stu_id: stuID,
-                                        unique_id: tiCode,
-                                        activity: actID,
-                                        status: 1,
-                                        seat: "",
-                                        cost: (staticACT.need_seat == 2 ? parseInt(staticACT.price) : 0)
-                                    }, function () {
-                                        usr_lock[stuID] = null;
-                                        presentTicket(msg, res, {unique_id: tiCode}, staticACT);
-                                        return;
-                                    });
-                            }, ss.substr(0, 8) + ss.substr(14), actName);
-                        });
-                }
+                presentTicket(msg, res, staticACT.user_map[stuID], staticACT);
+                usr_lock[stuID] = null;
+                lock.release('cache' + staticACT.activity_info.key);
             });
         });
     });
 };
+
 //========================================
 exports.check_reinburse_ticket = function (msg) {
     if (msg.MsgType[0] === "text")
@@ -279,6 +227,7 @@ exports.check_reinburse_ticket = function (msg) {
             return true;
     return false;
 };
+
 exports.faire_reinburse_ticket = function (msg, res) {
     var actName, openID;
     if (msg.Content[0] === "退票") {
@@ -297,73 +246,64 @@ exports.faire_reinburse_ticket = function (msg, res) {
     }, function (stuID) {
         verifyActivities(actName, function () {
             res.send(template.getPlainTextTemplate(msg, "目前没有符合要求的活动处于退票期。"));
-        }, function (actID) {
-            db[TICKET_DB].findAndModify(
-                {
-                    query: {stu_id: stuID, activity: actID, status: 1},
-                    update: {$set: {status: 0}}
-                }, function (err, result) {
-                    if (err || result == null) {
-                        res.send(template.getPlainTextTemplate(msg,
-                            "未找到您的抢票记录或您的票已经支付，退票失败。如为已支付票，请联系售票机构退还钱款后退票。"));
-                        return;
-                    }
-                    if (result.seat != "" && result.seat != null) {
-                        var incIndex = {};
-                        incIndex[result.seat] = 1;
-                        db[SEAT_DB].update({activity: actID},
-                            {
-                                $inc: incIndex
-                            }, function () {
-                                //Nothing? Oui, ne rien.
-                            });
-                    }
-
-                    fetchRemainTicket(actName, function () {
-                        db[ACTIVITY_DB].update({_id: actID},
-                            {
-                                $inc: {remain_tickets: 1}
-                            }, {multi: false}, function () {
-                                rem_cache[actName]++;
-                                tik_cache[actName].usrMap[stuID] = null;
-                                res.send(template.getPlainTextTemplate(msg, "退票成功。"));
-                                return;
-                            });
-                    });
-                });
+        }, function (actID, act) {
+            if (act.user_map[stuID].num == 0) {
+                res.send(template.getPlainTextTemplate(msg,
+                    "未找到您的抢票记录，退票失败。"));
+                return;
+            }
+            if (act.user_map[stuID].num != 1) {
+                res.send(template.getPlainTextTemplate(msg,
+                    "您在该活动中一共有" + act.user_map[stuID].num + "张票，请进入票夹单独退票。"));
+                return;
+            }
+            var ticket_id;
+            for (ticket_id in act.user_map[stuID].tickets) {
+            }
+            var ticket = act.user_map[stuID].tickets[ticket_id];
+            if (ticket.status != 1) {
+                res.send(template.getPlainTextTemplate(msg,
+                    "未找到您的抢票记录或您的票已经支付，退票失败。如为已支付票，请联系售票机构退还钱款后退票。"));
+                return;
+            }
+            lock.acquire('cache' + act.activity_info.key, function () {
+                if (ticket.seat != "" && ticket.seat != null) {
+                    act.seat_map[ticket.seat]++;
+                }
+                act.activity_info.remain_tickets++;
+                act.user_map[stuID] = null;
+                res.send(template.getPlainTextTemplate(msg, "退票成功。"));
+                lock.release('cache' + act.activity_info.key);
+            });
         });
     });
 };
+
 //========================================
 exports.check_list_ticket = function (msg) {
     if (msg.MsgType[0] === "text")
         if (msg.Content[0] === "查票")
             return true;
-    if (checker.checkMenuClick(msg) === basicInfo.WEIXIN_EVENT_KEYS['ticket_get'])
-        return true;
-    return false;
+    return (checker.checkMenuClick(msg) === basicInfo.WEIXIN_EVENT_KEYS['ticket_get']);
 };
+
 function renderTicketList(oneTicket, oneActivity, isSingle) {
     var ret = {};
-
     if (isSingle) {
         //Attentez: notify the user to select seat.
         ret[template.rich_attr.title] = "抢票成功！";
         ret[template.rich_attr.description] = oneActivity.name;
-    }
-    else
+    } else {
         ret[template.rich_attr.title] = oneActivity.name;
+    }
     ret[template.rich_attr.url] = urls.ticketInfo + "?ticketid=" + oneTicket.unique_id;
     ret[template.rich_attr.picture] = oneActivity.pic_url;
-
     return ret;
 }
-exports.faire_list_ticket = function (msg, res) {
-    var openID;
 
-    openID = msg.FromUserName[0];
+exports.faire_list_ticket = function (msg, res) {
+    var openID = msg.FromUserName[0];
     verifyStudent(openID, function () {
-        //WARNING: may change to direct user to bind
         res.send(needValidateMsg(msg));
     }, function (stuID) {
         db[TICKET_DB].find(
@@ -371,6 +311,16 @@ exports.faire_list_ticket = function (msg, res) {
                 stu_id: stuID,
                 $or: [{status: 1}, {status: 2}]
             }, function (err, docs) {
+                for (var x in current_activity) {
+                    var activity = current_activity[x];
+                    if (activity.status >= 0) {
+                        if (!activity.user_map[stuID]) {
+                            for (var y in activity.user_map[stuID].tickets) {
+                                docs.push(activity.user_map[stuID].tickets[y]);
+                            }
+                        }
+                    }
+                }
                 if (err || docs.length == 0) {
                     res.send(template.getPlainTextTemplate(msg, "没有找到属于您的票哦，赶快去抢一张吧！"));
                     return;
