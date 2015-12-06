02ee3757643715287ab1be97959a3b77ff345e3c
张哲 <zhangzhe19960115@gmail.com>
2015-12-06 08:18:16 +0800

    修复了抢票方式中的空格显示不正确问题。

--- public/js/activity_detail_extra.js
+++ public/js/activity_detail_extra.js
@@ -16,7 +16,7 @@ var get_ticket_method_title="抢票方式:";
 
 var get_ticket_method_content=
     ' 线上抢票，先到先得！<br><div style="height:1px"></div>'+
-    '· 在微信中直接回复<i style="color:#d0dd1e">"抢票　'+activity_key+' 抢票张数"</i>（此活动抢票张数不超过 <i style="color:#d0dd1e">'+activity_max_tickets+'</i> 张，若不填写抢票张数，默认抢 1 张）<br>'+
+    '· 在微信中直接回复<i style="color:#d0dd1e">"抢票 '+activity_key+' 抢票张数"</i>（此活动抢票张数不超过<i style="color:#d0dd1e">'+activity_max_tickets+'</i>张，若不填写抢票张数，默认抢 1 张）<br>'+
     '· 或者，点击"抢票"菜单下的<i style="color:#d0dd1e">"'+activity_key+'"</i>按钮（此方法只能抢 1 张）<br>';
 var activity_content_2 = '<b>'+activity_book_ticket_time+'</b>' + get_ticket_method_content + (!activity_seat_type?'':(activity_seat_type==1?'该活动分区域选座，抢到票后可进入查票界面进行选座。<br>':'抢到票后可进入查票界面进行选座。<br>'));
 get_ticket_method_content = '' + activity_book_ticket_time + get_ticket_method_content;
